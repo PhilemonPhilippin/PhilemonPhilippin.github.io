3bf29fb38563f137d09c2e9965a9c74f02830c93
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-06-13 09:52:50 +0200

    freemeal v2.0.0 ajoutée

--- becode-exercises/freemeal/app.js
+++ becode-exercises/freemeal/app.js
@@ -1,6 +1,12 @@
+//#region DOM elements.
+// Search.
 const searchButton = document.querySelector(".btn-search");
-const grid = document.querySelector(".grid");
+const searchInput = document.querySelector("#search");
 const searchResultText = document.querySelector(".search-result-text");
+// Grid.
+const grid = document.querySelector(".grid");
+
+// Modal.
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".btn-close");
@@ -8,7 +14,10 @@ const mealTitle = document.querySelector(".meal-title");
 const mealInstructions = document.querySelector(".meal-instructions");
 const mealThumb = document.querySelector(".meal-thumb");
 const mealIngredients = document.querySelector(".meal-ingredients");
+//#endregion
 
+//#region Events
+// Close modal AND overlay events.
 closeModalBtn.addEventListener("click", () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
@@ -19,46 +28,93 @@ overlay.addEventListener("click", () => {
   overlay.classList.add("hidden");
 });
 
+// Search for meals events.
 searchButton.addEventListener("click", searchMeals);
 
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchMeals();
+  }
+});
+//#endregion
+
+//#region Search meal functionality
 async function searchMeals() {
-  const input = document.querySelector("#search");
-  const searchString = input.value;
+  const searchString = searchInput.value;
   const response = await getMeals(searchString);
   updateGrid(response);
   updateSearchResultText(searchString);
 }
 
-function updateSearchResultText(text) {
-  searchResultText.innerText = `Results for "${text}" :`;
+async function getMeals(searchString) {
+  const uri = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
+  const response = await fetch(uri);
+  const json = await response.json();
+  return json["meals"];
 }
 
 function updateGrid(meals) {
   grid.innerHTML = "";
+
   meals.forEach((meal, index) => {
-    const thumb = meal["strMealThumb"];
-    let imageElem = document.createElement("img");
-    imageElem.src = thumb;
-    const div = document.createElement("div");
-    div.classList.add(`grid-item-${index}`);
-    div.classList.add("grid-item");
-    div.appendChild(imageElem);
-    grid.appendChild(div);
-    const title = meal["strMeal"];
-    let titleElem = document.createElement("h4");
-    titleElem.classList.add("title");
-    titleElem.innerText = title;
-    div.appendChild(titleElem);
-    div.addEventListener("click", () => openModal(meal));
+    const imageElem = createThumbImg(meal["strMealThumb"]);
+    const gridItem = createGridItem(index);
+
+    gridItem.appendChild(imageElem);
+
+    const titleElem = createTitle(meal["strMeal"]);
+    gridItem.appendChild(titleElem);
+
+    addEvents(gridItem, meal, titleElem);
+
+    grid.appendChild(gridItem);
+  });
+}
+
+function createThumbImg(mealThumb) {
+  let imageElem = document.createElement("img");
+  imageElem.src = mealThumb;
+  return imageElem;
+}
+
+function createGridItem(index) {
+  const div = document.createElement("div");
+  div.classList.add(`grid-item-${index}`);
+  div.classList.add("grid-item");
+  return div;
+}
+
+function createTitle(mealTitle) {
+  let titleElem = document.createElement("h4");
+  titleElem.classList.add("title");
+  titleElem.classList.add("white-text");
+  titleElem.innerText = mealTitle;
+  return titleElem;
+}
+
+function addEvents(gridItem, meal, titleElem) {
+  gridItem.addEventListener("click", () => openModal(meal));
+  gridItem.addEventListener("mouseover", () => {
+    titleElem.style.color = "black";
+  });
+  gridItem.addEventListener("mouseout", () => {
+    titleElem.style.color = "white";
   });
 }
 
 function openModal(meal) {
-  const thumb = meal["strMealThumb"];
-  const title = meal["strMeal"];
-  const instructions = meal["strInstructions"];
+  mealIngredients.innerHTML = "";
+
+  mealTitle.innerText = meal["strMeal"];
+  mealThumb.setAttribute("src", meal["strMealThumb"]);
+  mealInstructions.innerText = meal["strInstructions"];
+
+  // Declare array of ingredient objects.
   const ingredients = [];
+
+  // Build the array.
   for (let i = 1; i <= 20; i++) {
+    // End loop if there is no more ingredient in the array.
     if (
       meal[`strIngredient${i}`] === "" ||
       meal[`strIngredient${i}`] === null
@@ -72,23 +128,18 @@ function openModal(meal) {
     }
   }
 
-  mealIngredients.innerHTML = "";
+  // Use the array to add each list item.
   ingredients.forEach((ingredient) => {
     const listItem = document.createElement("li");
     listItem.innerText = `${ingredient.quantity} of ${ingredient.name}`;
     mealIngredients.appendChild(listItem);
   });
-  mealTitle.innerText = title;
-  mealInstructions.innerText = instructions;
-  mealThumb.setAttribute("src", thumb);
 
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 }
 
-async function getMeals(searchString) {
-  const uri = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
-  const response = await fetch(uri);
-  const json = await response.json();
-  return json["meals"];
+function updateSearchResultText(text) {
+  searchResultText.innerText = `Results for "${text}" :`;
 }
+//#endregion
